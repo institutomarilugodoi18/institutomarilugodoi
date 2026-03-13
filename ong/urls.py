8069732b9cfdbb33b9a9c8154552bfac36d4238e
frdestudos <173936236+frdestudos@users.noreply.github.com>
2026-03-13 00:11:57 -0300

    fix(media): serve uploads em producao

--- ong/urls.py
+++ ong/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from .views import index  # importa a view criada
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,6 +28,5 @@ urlpatterns = [
     path('voluntarios/', include('apps.voluntarios.urls')),
     path('animais/', include('apps.animais.urls')),
     path('relatorios/', include('apps.relatorios.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
