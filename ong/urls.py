690f60f0376c01e1dd7e085ef2baff92d05c82ec
frdestudos <173936236+frdestudos@users.noreply.github.com>
2026-03-12 23:40:53 -0300

    fix(urls): upload media

--- ong/urls.py
+++ ong/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
     path('relatorios/', include('apps.relatorios.urls')),
 ]
 
-# Servir uploads (media) apenas em desenvolvimento
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
